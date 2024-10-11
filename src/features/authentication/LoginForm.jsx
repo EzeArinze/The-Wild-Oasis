26fbafe8217460c777eb07e32521bf1f2717dda6
Arinze-Eze <nuelrinz@gmail.com>
2024-10-11 10:25:12 +0100

    building for production

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -9,8 +9,8 @@ import { useLogin } from "./useLogin";
 function LoginForm() {
   const { login, isLogingIn } = useLogin();
 
-  const [email, setEmail] = useState("nuel@example.com");
-  const [password, setPassword] = useState("pass2244");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
